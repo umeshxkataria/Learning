77b3c0ea5b40014d9516ed18af407b70c96730e2
Umesh  Kataria <120646442+umeshxkataria@users.noreply.github.com>
2025-06-06 11:02:35 +0530

    Javascript learning

---
+++ Javascript/Day-1/Day-1.js
@@ -0,0 +1,8 @@
+// Resources :- https://youtu.be/ajdRvxDWH4w?si=_OPYo9V-QruxBuc6 
+
+
+
+console.log("Day 1 of learning JavaScript: 04/06/2025");
+console.log("Today, I will focus on understanding the basics of JavaScript.");
+name = "John Doe";
+console.log("My name is " + name + ".");
---
+++ Javascript/Day-2/Day2.js
@@ -0,0 +1,82 @@
+// console.log("Day 2 of Learning JavaScript");
+
+//Q1:->
+// console.log("Q.1 Take input from and check if it is a mutliple of 5 or not");
+// let num = prompt("Enter a number: ");
+// if (num%5 == 0){
+//     console.log("The number is a multiple of 5");   
+// }else{
+//     console.log("Not a multiple of 5")};
+    
+
+//Q2:->
+// console.log("Q.2 Take input from student and grade them based on their marks");
+// let marks = prompt("Enter your makrs: ");
+// if (marks >= 90 && marks <= 100){
+//     console.log("You got an A+ grade");
+// }
+// if (marks >= 80 && marks < 90){
+//     console.log("You got an A grade");
+// }  
+// if (marks >= 70 && marks < 80){
+//     console.log("You got a B grade");
+// }
+// if (marks >= 60 && marks < 70){
+//     console.log("You got a C grade");
+// }
+// if (marks >= 50 && marks < 60){
+//     console.log("You got a D grade");
+// }
+// if (marks >= 40 && marks < 50){
+//     console.log("You got an E grade");
+// }   
+// if (marks < 40){
+//     console.log("You got an F grade");
+// }
+
+
+
+
+// // var a=88
+// // var a=899
+// // var a=888
+
+// // let b=111
+// // //let b=222
+// // //let b=333
+
+// // const c=555
+// // //const c=666 // This will throw an error because 'const' cannot be redeclared
+// // // const c=666 // This will throw an error because 'const' cannot be 
+
+// // /*sum of 2 numbers*/
+// // function sum(x,y){
+// //     return x+y;
+// // }
+
+// // console.log(sum(2,3));
+
+// // a=1
+// // a++
+// // console.log(a); // 2
+// // a--
+// // console.log(a); // 1
+// // ++a; // This is a pre-increment, but it does nothing here
+// // console.log(a); // 2
+
+
+
+
+// // a=5
+// // b="5"
+// // console.log("a !== b"  , a !== b); // true, because '===' checks value and type
+
+
+// // console.log("Program to check if a number is even or odd");
+// // let number = 9;
+// // if (number %2 === 0){
+// //     console.log("The number is even");
+// // }else{
+// //     console.log("The number is odd");
+// // }
+
